5a4b3bf4ea7a1e6b374a382d765e0e9cb045816a
S-i-d-R <Sid.R@bk.ru>
2022-09-13 20:58:32 +0500

    Task_4

---
+++ Семинар_1/Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите третье число: ");
+int numberС = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max) max = numberB;
+if (numberС > max) max = numberС;
+
+Console.Write ("Максимальное число: ");
+Console.WriteLine (max);
