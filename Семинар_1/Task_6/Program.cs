6cc7f85765d1d18510d78ae8f628dcfeef371295
S-i-d-R <Sid.R@bk.ru>
2022-09-13 21:32:53 +0500

    Task_6

---
+++ Семинар_1/Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remains = number % 2;
+
+if (remains == 0)
+{
+ Console.WriteLine ("Ваше число четное");  
+}
+else
+{
+ Console.WriteLine ("Ваше число нечетное");
+}
