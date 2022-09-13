d9f34bd970303e315fd826cddeffa6d8dcc4ecbf
S-i-d-R <Sid.R@bk.ru>
2022-09-13 20:14:01 +0500

    Task_2

---
+++ Семинар_1/Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine ("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == numberB)
+{
+  Console.WriteLine ("Числа равны");
+}
+else
+{
+if (numberA > numberB)
+{
+  Console.Write ("Большее число: ");
+  Console.WriteLine (numberA); 
+  Console.Write ("Меньшее число: ");
+  Console.WriteLine (numberB); 
+}
+else 
+{
+  Console.Write ("Большее число: ");
+  Console.WriteLine (numberB); 
+  Console.Write ("Меньшее число: ");
+  Console.WriteLine (numberA);   
+}
+}
