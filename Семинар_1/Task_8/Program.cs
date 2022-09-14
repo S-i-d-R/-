6ed4709f114783fd75993a1b0aaa0a156d82694c
S-i-d-R <Sid.R@bk.ru>
2022-09-14 20:55:30 +0500

    Task_8

---
+++ Семинар_1/Task_8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int count = numberN;
+
+while (count >= 1)
+{
+int remains = count % 2;
+if (remains == 0) 
+{    
+Console.WriteLine (count);
+}
+count = count - 1;
+}
