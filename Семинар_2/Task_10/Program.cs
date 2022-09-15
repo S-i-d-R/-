81d5d8d535a09fc42db9738c01d6ccb134bce8cb
S-i-d-R <Sid.R@bk.ru>
2022-09-15 23:22:49 +0500

    Task_10

---
+++ Семинар_2/Task_10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 99 && number < 1000)
+{
+    int result1 = number / 10;
+    int result2 = result1 % 10;
+    Console.WriteLine (result2);
+}
+else
+{
+   Console.WriteLine ("Вы ввели не трехзначное число!"); 
+}
