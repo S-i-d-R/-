bda40ad3e8555fe812a44b0c0c568c98c7f8d867
S-i-d-R <Sid.R@bk.ru>
2022-09-16 23:45:14 +0500

    Task_15

---
+++ Семинар_2/Task_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Введите порядковый номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0 && number < 6)
+{
+ Console.WriteLine ("Это будний день.");
+}
+if (number > 5 && number < 8)
+{ 
+Console.WriteLine ("Это выходной день.");
+}
+if (number > 7 ^ number < 1)
+{
+ Console.WriteLine ("Такого порядкового номера дня недели, НЕТ.");
+}
