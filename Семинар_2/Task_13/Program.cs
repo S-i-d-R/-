0fd814396cfae579766a9790c4e90de02990f649
S-i-d-R <Sid.R@bk.ru>
2022-09-16 22:46:58 +0500

    Task_13

---
+++ Семинар_2/Task_13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = number;
+
+while (count > 999)
+{
+ count = count / 10;
+}
+if (count <= 999 && count > 99)
+{
+ int number3 = count % 10;
+ Console.WriteLine ($"Третье число: {number3}"); 
+}
+else
+{
+ Console.WriteLine ("Третьего числа нет.");   
+}
