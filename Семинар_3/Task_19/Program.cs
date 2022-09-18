1d697b239d36c06dde883be651ba084009d41632
S-i-d-R <Sid.R@bk.ru>
2022-09-18 12:29:32 +0500

    Task_19

---
+++ Семинар_3/Task_19/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine ("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9999 && number < 100000)
+{
+    int number1 = number / 10000;
+    int number2 = (number / 1000) % 10;
+    int number4 = (number / 10) % 10;
+    int number5 = number % 10;
+
+    if (number1 == number5 & number2 == number4)
+{
+    Console.WriteLine ("Введенное число - это полиндром.");
+}
+    else
+    {
+            Console.WriteLine ("Введенное число - это НЕ полиндром.");
+    }
+}
+else
+{
+   Console.WriteLine ("Вы ввели не пятизначное число!"); 
+}
