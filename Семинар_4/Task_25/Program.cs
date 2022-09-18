a749dd142a520fbc5778b680f32be5c2af7adf62
S-i-d-R <Sid.R@bk.ru>
2022-09-18 22:06:23 +0500

    Task_25

---
+++ Семинар_4/Task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numberA = ReadNumber("Введите положительное число A:");
+int numberB = ReadNumber("Введите положительное число B:");
+
+int i = numberB;
+int result = 1;
+
+if (numberA > 0 & numberB > 0)
+{
+for (i = numberB; i > 0; i--)
+{
+    result = result * numberA;
+}
+Console.WriteLine($"Результат: {result}");
+}
+else
+{
+ Console.WriteLine("Вы ввели отрицательное число!");   
+}
