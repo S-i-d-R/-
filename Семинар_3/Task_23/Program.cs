9c931a4dfb9e19055dbd192660f604a99d0dd53d
S-i-d-R <Sid.R@bk.ru>
2022-09-18 13:16:27 +0500

    Task_23

---
+++ Семинар_3/Task_23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите положительное число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 0)
+{
+    for (int i = 1; i <= N; i++)
+    {
+      Console.Write(i * i * i + " ");  
+    }
+}
+else
+{
+  Console.WriteLine("Введено отрицательное число!");  
+}
