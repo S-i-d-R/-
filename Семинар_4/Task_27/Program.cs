16d4d86c64f8868ea3dfea4a16b00f8174a528e6
S-i-d-R <Sid.R@bk.ru>
2022-09-18 23:01:32 +0500

    Task_27

---
+++ Семинар_4/Task_27/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine ("Введите положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = number;
+int sum = 0;
+
+if (number > 0)
+{
+while (i > 0)
+{
+sum = sum + i % 10;
+i = i / 10;
+}
+Console.WriteLine($"Сумма цифр введенного числа: {sum}");
+}
+else
+{
+ Console.WriteLine("Вы ввели отрицательное число!");   
+}
