ab4e6a46687955850a2b947edbe58b993614647b
S-i-d-R <Sid.R@bk.ru>
2022-09-19 19:42:45 +0500

    Task_29

---
+++ Семинар_4/Task_29/Program.cs
@@ -0,0 +1,10 @@
+﻿int [] array = new int [8];
+
+Console.WriteLine ("Введите восемь элементов массива: ");
+
+for (int i = 0; i < array.Length; i++)
+{
+array [i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write ("Ваш массив: ");
+Console.WriteLine (string.Join (", ", array));
