edfcfb0d930f4ef757f2c9bddcce1795bc78fc50
S-i-d-R <Sid.R@bk.ru>
2022-09-29 22:54:41 +0500

    Task_34

---
+++ Семинар_5/Task_34/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int [size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+int[] array = GetArray(6, 100, 999);
+Console.WriteLine("Задан массив:");
+Console.WriteLine(string.Join(", ", array));
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+if (array[i] % 2 == 0)
+{
+    count = count + 1;
+}
+Console.WriteLine("Количество четных чисел в массиве:");
+Console.WriteLine(count);
