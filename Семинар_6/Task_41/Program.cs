f69dbb108e0f268e34eb00a05aad7bdac531a06f
S-i-d-R <Sid.R@bk.ru>
2022-10-07 21:26:34 +0500

    Task_41

---
+++ Семинар_6/Task_41/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите числа, в конце введите слово: stop");
+
+int countPositivNumber = 0;
+while(true)
+{
+    string answer = Console.ReadLine();
+    if(answer == "stop")
+    {
+        break;
+    }
+    else
+    {
+        int Number = Convert.ToInt32(answer);
+        if(Number > 0)
+        {
+            countPositivNumber++;
+        }
+    }
+}
+Console.WriteLine($"Колличество чисел больше нуля = {countPositivNumber}");
