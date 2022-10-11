0984bf8d41764035bbde1e793df824f3b474f8f2
S-i-d-R <Sid.R@bk.ru>
2022-10-11 21:50:11 +0500

    Task_52

---
+++ Семинар_7/Task_52/Program.cs
@@ -0,0 +1,40 @@
+﻿int[,] GetMatrix(int rowsCount = 5, int columnCount = 5, int leftRange = 0, int rightRange = 50)
+{
+    int[,] matrix = new int[rowsCount, columnCount];
+    Random rand = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array = GetMatrix();
+PrintMatrix(array);
+Console.WriteLine();
+void ArithmeticMeanColumn(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double a = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            a = array[i, j] + a;
+        }
+        Console.Write($"{a / array.GetLength(0)}  ");
+    }
+}
+ArithmeticMeanColumn(array);
