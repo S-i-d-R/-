15ede58f2feafa8648002222b996f69c0f2d1345
S-i-d-R <Sid.R@bk.ru>
2022-10-13 22:01:30 +0500

    Task_54

---
+++ Семинар_8/Task_54/Program.cs
@@ -0,0 +1,57 @@
+﻿int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[,] GetMatrix(int rowsCount, int columnCount, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnCount];
+    Random rand = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange +1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+void ArrangeMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+      {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+          int temp = matrix[i, k + 1];
+          matrix[i, k + 1] = matrix[i, k];
+          matrix[i, k] = temp;
+        }
+      }
+    }
+  }
+}
+
+int m = ReadNumber("Введите колличество строк");
+int n = ReadNumber("Введите колличество столбцов");
+int[,] matr = GetMatrix(m,n);
+Console.WriteLine("Случайный массив:");
+PrintMatrix(matr);
+Console.WriteLine();
+Console.WriteLine("Упорядоченный массив:");
+ArrangeMatrix(matr);
+PrintMatrix(matr);
