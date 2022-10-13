418363f8fbf32c88a9a769c08591ad0dda9a1b74
S-i-d-R <Sid.R@bk.ru>
2022-10-13 23:28:26 +0500

    Task_60

---
+++ Семинар_8/Task_60/Program.cs
@@ -0,0 +1,36 @@
+﻿int number = 10;
+void GetArray(int[,,] array) 
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = number;
+                number++;
+            }
+        }
+    }
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i}, {j}, {k}) ");
+            }
+
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[, ,] array = new int[2, 2, 2];
+GetArray(array);
+PrintArray(array);
