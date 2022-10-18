c36f6bd1912aeecddaecc8faa92f523eb002ca5e
S-i-d-R <Sid.R@bk.ru>
2022-10-18 21:33:39 +0500

    Task_64

---
+++ Семинар_9/Task_64/Program.cs
@@ -0,0 +1,27 @@
+﻿int ReadNumber(string message)  
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNTo1(int n) 
+{   
+    if(n < 0)
+    {
+        Console.WriteLine("Число не натуральное!");
+        return;
+    }
+    if(n == 0)
+    {
+        return;
+    }
+    else
+    {   
+        Console.Write(n + " ");
+        PrintNTo1(n - 1);
+
+    }
+}
+
+int numberN = ReadNumber("Введите число: ");
+PrintNTo1(numberN);
