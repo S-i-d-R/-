f0ad044d8a96806671dd26b20cf3a25f1911a0ad
S-i-d-R <Sid.R@bk.ru>
2022-10-18 21:55:46 +0500

    Task_68

---
+++ Семинар_9/Task_68/Program.cs
@@ -0,0 +1,37 @@
+﻿int ReadNumber(string message) 
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int m = ReadNumber("Введите первое  число: ");
+int n = ReadNumber("Введите второе число: ");
+
+int FunctionAkkerman(int m, int n) 
+{
+   if(n < 0 || m < 0)
+    {
+        Console.WriteLine("Введено не положительное число!");
+        return 0;
+    }
+    if(m == 0) 
+    {
+        return n + 1;
+    }
+    if(m > 0 && n == 0)
+    {
+        return FunctionAkkerman(m - 1, 1);
+    }
+    else
+    {
+        return FunctionAkkerman(m - 1, FunctionAkkerman(m , n - 1));
+    }
+}
+
+void Akkerman(int m, int n)  
+{   
+    Console.WriteLine();
+    Console.Write($"Функция Аккермана равна: ");
+    Console.WriteLine(FunctionAkkerman(m, n));
+}
+
+Akkerman(m, n);
